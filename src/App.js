54c6b552b9ea85c25f7036e3ec9af2526f9c5da2
1804011 <sharifsharifulislam061@gmail.com>
2022-03-25 21:37:48 +0600

    project initialized

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+function App() {
+	return <div></div>;
+}
+
+export default App;

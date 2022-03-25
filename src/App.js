e4fc07db7579c934ec11119dfbfb2d081a567729
1804011 <sharifsharifulislam061@gmail.com>
2022-03-25 22:13:46 +0600

    fake data created

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
-
+import dell1 from "./Images/dell1.jfif";
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<img src={dell1} alt="" />
+		</div>
+	);
 }
 
 export default App;
